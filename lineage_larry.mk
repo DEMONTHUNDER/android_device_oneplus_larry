4d3ace7d5051e4afb696abf5a7125b6989f09df9
DEMONTHUNDER <110889824+DEMONTHUNDER@users.noreply.github.com>
2026-09-04 22:22:48 +0530

    Update lineage_larry.mk

--- lineage_larry.mk
+++ lineage_larry.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023-2026 The LineageOS Project & RisingOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,18 +9,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff
+# Inherit common RisingOS environment
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from larry device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_larry
+PRODUCT_NAME := rising_larry
 PRODUCT_DEVICE := larry
 PRODUCT_BRAND := oneplus
 PRODUCT_MODEL := CPH2467
 PRODUCT_MANUFACTURER := oneplus
+
+# RisingOS Maintainer Tag
+RISING_MAINTAINER := DemonThunder
+
+# UI & Performance Features
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_HARDWARE_QCOM_PROPRIETARY := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
